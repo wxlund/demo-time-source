56ae4d06b4f40d01c6c7738adb157fb3a476e09d
Wayne Lund <wlund@Waynes-MacBook-Pro-8.local>
2017-03-06 15:35:23 -0800

    1st commit

--- src/main/java/com/example/dataflow/DemoTimeSourceApplication.java
+++ src/main/java/com/example/dataflow/DemoTimeSourceApplication.java
@@ -2,8 +2,10 @@ package com.example.dataflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(org.springframework.cloud.stream.app.time.source.TimeSourceConfiguration.class)
 public class DemoTimeSourceApplication {
 
 	public static void main(String[] args) {
